1bba80d517f98697e43503288d94b136d420c674
discokittens <discokitten@163.com>
2022-01-13 18:33:27 +0800

    sh

---
+++ cron.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+if ! grep -q '/sbin/reboot' /etc/crontab; then
+	    echo "0 9 * * * root /sbin/reboot" >> /etc/crontab 
+	        echo "0 18 * * * root /sbin/reboot" >> /etc/crontab 
+fi
