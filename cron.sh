b90e0e73c229f9864c0a1f5296c344dbd66ac04c
discokittens <92132370+discokittens@users.noreply.github.com>
2022-01-13 20:30:11 +0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -2,6 +2,6 @@
 set -e
 
 if ! grep -q '/sbin/reboot' /etc/crontab; then
-	    echo "0 9 * * * root /sbin/reboot" >> /etc/crontab 
-	        echo "0 18 * * * root /sbin/reboot" >> /etc/crontab 
+    echo "0 9 * * * root /sbin/reboot" >> /etc/crontab 
+    echo "0 18 * * * root /sbin/reboot" >> /etc/crontab 
 fi
